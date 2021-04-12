2cdeb52bd5fdddec6ec2e0c5eeeed2f652815d92
trakzter <82202585+trakzter@users.noreply.github.com>
2021-04-12 11:22:49 +0200

    adding function

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static int result;
+    public static void main(String[] args) {
+
+	    int input = 1;
+        result = addfunction(input);
+
+    }
+
+    public static int addfunction(int value){
+
+        value += 1;
+        return value;
+    }
+}
